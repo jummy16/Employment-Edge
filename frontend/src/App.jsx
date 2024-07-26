4d889badc7e9877456c4e90fe3b7bf369f8700bb
Akinwumi sehinde <102046091+Sahar009@users.noreply.github.com>
2024-07-26 09:34:33 +0100

    index style added
    take your default styles from index.css , Routing fixed route your pages using Routes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <BrowserRouter>
+    {/* Let your routes come in here */}
+    <h1>Employment edge!!!</h1>
+    </BrowserRouter>
+  )
+}
+
+export default App
